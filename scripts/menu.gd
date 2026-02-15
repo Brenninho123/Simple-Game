8c14da8a30de40a9b96c0eb776306b99f1292525
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-02-15 16:30:37 -0300

    Atualizar o menu.gd
    Animação Galera

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,12 +1,31 @@
 extends Node2D
 
-# Funções conectadas aos botões
+var glow_timer = 0
+var glow_direction = 1
+
 func _ready():
+    # Conectar os botões
     $StartButton.connect("pressed", self, "_on_StartButton_pressed")
     $QuitButton.connect("pressed", self, "_on_QuitButton_pressed")
+    
+    # Iniciar música
+    $MusicPlayer.play()
+    
+    # Configura Label central
+    $TitleLabel.modulate = Color(1,1,1)
+
+func _process(delta):
+    # Efeito de brilho pulsante no título
+    glow_timer += delta * glow_direction
+    if glow_timer > 1:
+        glow_timer = 1
+        glow_direction = -1
+    elif glow_timer < 0:
+        glow_timer = 0
+        glow_direction = 1
+    $TitleLabel.modulate = Color(1,1,1).linear_interpolate(Color(1,1,0.5), glow_timer)
 
 func _on_StartButton_pressed():
-    # Troca para a cena principal do jogo
     get_tree().change_scene("res://scenes/main.tscn")
 
 func _on_QuitButton_pressed():
